3f26473701e8c35b4ef017b61b20dd9420402e3d
Stephen Way <way.stephen@gmail.com>
2015-12-08 15:13:56 -0800

    Added commands

---
+++ bin/install.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+var shell = require("shelljs");
+var userArgs = process.argv.slice(2);
+var searchParam = userArgs[0];
+
+var exec = require('child_process').exec;
+var child = exec('npm install --save-dev stylep-' + searchParam, function(err, stdout, stderr) {
+        if (err) throw err;
+        console.log(stdout);
+});
