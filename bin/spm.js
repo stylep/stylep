bf21076fd545610bfbf48ebdf34d1efacd3d6c7b
Stephen Way <way.stephen@gmail.com>
2015-12-21 11:43:45 -0800

    Combined all into subcommand enabled spm

---
+++ bin/spm.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+var yargs = require("yargs");
+var shell = require("shelljs");
+var userArgs = process.argv.slice(3);
+
+if (process.argv.length < 3) {
+        console.log('spm: missing argument');
+        console.log();
+        process.exit(1);
+}
+
+for (var i = 0; i < userArgs.length; i++) {
+        if (userArgs[i][0] !== "-") {
+                userArgs[i] = 'stylep-' + userArgs[i];
+        }
+}
+
+var searchString = userArgs.join(' ');
+
+var argv = yargs.usage("$0 command")
+        .command("install", "installs style patterns to your project", function(yargs) {
+                shell.exec("npm install --save-dev " + searchString, function(err, stdout, stderr){});
+        })
+        .alias("i", "install")
+        .command("uninstall", "uninstalls style patterns from your project", function(yargs) {
+                shell.exec("npm uninstall --save-dev " + searchString, function(err, stdout, stderr){});
+        })
+        .alias("u", "uninstall")
+        .command("list", "lists out all installed style patterns in your project", function(yargs) {
+                shell.exec("npm list --depth=0 2>/dev/null", function(err, stdout, stderr){});
+        })
+        .alias("ls", "list")
+        .help("h")
+        .alias("h", "help")
+        .argv;
+
