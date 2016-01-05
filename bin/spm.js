592852445e6c04a2d07f515a5b327b82638732ef
Stephen Way <way.stephen@gmail.com>
2016-01-05 14:22:43 -0800

    Remove merge conflict

--- bin/spm.js
+++ bin/spm.js
@@ -19,29 +19,6 @@ for (var i = 0; i < userArgs.length; i++) {
 var searchString = userArgs.join(' ');
 
 var argv = yargs.usage("$0 command")
-<<<<<<< dbff8e9a619b4c8e5ad20df18efdd43e1544bf50
-        .command("install", "installs style patterns to your project", function(yargs) {
-                shell.exec("npm install --save-dev " + searchString, function(err, stdout, stderr){});
-        })
-        .command("i", "Shorthand for install", function(yargs) {
-                shell.exec("npm install --save-dev " + searchString, function(err, stdout, stderr){});
-        })
-        .command("uninstall", "uninstalls style patterns from your project", function(yargs) {
-                shell.exec("npm uninstall --save-dev " + searchString, function(err, stdout, stderr){});
-        })
-        .command("un", "Shorthand for uninstall", function(yargs) {
-                shell.exec("npm uninstall --save-dev " + searchString, function(err, stdout, stderr){});
-        })
-        .command("list", "lists out all installed style patterns in your project", function(yargs) {
-                shell.exec("npm list --depth=0 2>/dev/null | grep stylep", function(err, stdout, stderr){});
-        })
-        .command("ls", "Shorthand for ls", function(yargs) {
-                shell.exec("npm list --depth=0 2>/dev/null | grep stylep", function(err, stdout, stderr){});
-        })
-        .help("h")
-        .alias("h", "help")
-        .argv;
-=======
 .command("install", "installs style patterns to your project", function(yargs) {
   shell.exec("npm install --save-dev " + searchString, function(err, stdout, stderr){});
 })
@@ -51,7 +28,7 @@ var argv = yargs.usage("$0 command")
 .command("uninstall", "uninstalls style patterns from your project", function(yargs) {
   shell.exec("npm uninstall --save-dev " + searchString, function(err, stdout, stderr){});
 })
-.command("u", "Shorthand for uninstall", function(yargs) {
+.command("un", "Shorthand for uninstall", function(yargs) {
   shell.exec("npm uninstall --save-dev " + searchString, function(err, stdout, stderr){});
 })
 .command("list", "lists out all installed style patterns in your project", function(yargs) {
@@ -60,15 +37,7 @@ var argv = yargs.usage("$0 command")
 .command("ls", "Shorthand for ls", function(yargs) {
   shell.exec("npm list --depth=0 2>/dev/null | grep stylep", function(err, stdout, stderr){});
 })
-.command("new", "starts a new style pattern project in a new directory", function(yargs) {
-  if ( typeof searchString !== 'undefined' ) {
-    shell.exec("mkdir" + searchString + "&& curl -L https://github.com/stylep/stylep-template/tarball/master | tar -zx - -C" + searchString + "--strip-components=1", function(err, stdout, stderr){});
-  } else {
-    shell.exec("mkdir stylp-new && curl -L https://github.com/stylep/stylep-template/tarball/master | tar -zx - -C stylep-new --strip-components=1", function(err, stdout, stderr){});
-  }
-})
 .help("h")
 .alias("h", "help")
 .argv;
->>>>>>> Added new command
 
